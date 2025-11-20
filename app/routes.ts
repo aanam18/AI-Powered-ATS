47bca08cccedf9b4971c3cd05b583e50006534a4
docker-testapp <shaikhflora65@gmail.com>
2025-11-21 10:42:48 +1300

    Implement wipe functionality

--- app/routes.ts
+++ app/routes.ts
@@ -5,4 +5,5 @@ export default [
     route('/auth', 'routes/auth.tsx'),
     route('/resume/:id', 'routes/resume.tsx'),
     route('/upload', 'routes/upload.tsx'),
+    route('/wipe', 'routes/wipe.tsx'),
 ] satisfies RouteConfig;
